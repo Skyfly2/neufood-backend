afc0ebd68606f543e79517a12c038b7a6e5dec49
Asher Hamrick <ashhamrick2@gmail.com>
2023-09-21 15:34:44 -0400

    open endpoints to all

--- route.js
+++ route.js
@@ -19,7 +19,7 @@ var recipes = require("./routes/recipes/recipes").recipes;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
