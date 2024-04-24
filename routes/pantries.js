d56bba0dec1ba40a9afda70bcd65d9fe26c3edc3
Thor Long <thorlong17@outlook.com>
2024-04-24 13:06:41 -0400

    pantry get by pantryId

--- routes/pantries.js
+++ routes/pantries.js
@@ -9,11 +9,60 @@ router.post('/', async (req, res) => {
         const { name, ownerId } = req.body //extract name and description from request body
 
         //check if provided
+        if (!name || !ownerId) {
+            return res.status(400).json({error: 'Name of pantry and ownerId are required.'});
+        }
+
+        //generate unique pantryId
+        const pantryId = await generateUniquePantryId();
+
+        const pantry = new Pantries({
+            pantryId: pantryId,
+            name: name,
+            ownerId: ownerId,
+        });
+
+        //save pantry to db
+        const savedPantry = await pantry.save();
+
+        // send saved pantry as response
+        res.status(201).json(savedPantry);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error creating a new pantry' });
     }
 });
+
+// GET route to retrieve a pantry by pantryId => 
+//      this would be used for all interaction for getting information from pantries.
+//      Ex: want to display all ingredients in a pantry? GET by pantryId and sort through ingredients array.
+//      "but thor how do we get the pantryIds???" they should be in a nice array in the user route
+router.get('/:pantryId', async (req, res) => {
+    try {
+        const { pantryId } = req.params;
+        
+        //find pantry by pantryId
+        const pantry = await Pantries.findOne({ pantryId });
+
+        //check if exists
+        if (!pantry) {
+            return res.status(404).json({ error: 'Pantry not found.'});
+        }
+
+        //send pantry as object as response
+        res.json(pantry);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error fetching from pantry' });
+    }
+});
+// DELETE route to delete pantry by pantryId
+
+// PUT route to add ingredient(s) to a pantry
+// DELETE route to delete ingredient(s) from a pantry
+
+// PUT route to add collaborator(s) to a pantry
+// DELETE route to delete collaborator(s) from a pantry
 
 
 
@@ -43,3 +92,7 @@ async function generateUniquePantryId() {
         throw error;
     }
 }
+
+/**
+ * Question: do we ever need to change the pantry, name, or ownerId of the pantry? no I don't think we let them change name of pantry
+ */
