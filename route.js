6944cce8da8b985f819d6d47eed017ccc96a06c4
Mike Hazaz <mmh524@lehigh.edu>
2024-04-23 22:11:46 -0400

    mongo connection setup and established in route.js, added Counter schema/model for ID's for things, forgot this was a handy thing to have and will add to UML diagram, added routes for badges and allergies as well

--- route.js
+++ route.js
@@ -1,22 +1,31 @@
-var express = require("express");
-var app = express();
-
-const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const allergiesRoutes = require("./routes/allergies");
+const badgesRoutes = require("./routes/badges");
+const dotenv = require('dotenv');
 dotenv.config();
 
-const { google } = require("googleapis");
-const client = new google.auth.OAuth2();
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// Check if MongoDB URI is provided
+if (!process.env.MONGODB_URI) {
+  console.error('MongoDB URI not found. Please make sure to set the MONGODB_URI environment variable.');
+  process.exit(1);
+}
 
-const url = process.env.MONGODB_URI;
-const jwt = require("jsonwebtoken");
+// Connect to MongoDB using Mongoose
+mongoose.Promise = global.Promise;
 
-var express = require("express");
-var app = express();
-var recipes = require("./routes/recipes/recipes").recipes;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('MongoDB connection error. Please make sure MongoDB is running.');
+  process.exit();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Enable CORS
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -25,113 +34,9 @@ app.use(function (req, res, next) {
   next();
 });
 
-
-
-
-//google auth
-const users = []; //store user to mongo db or whatever.
-const one_D_array = [];
-function upsert(array, item) {
-  const i = array.findIndex((_item) => _item.email === item.email);
-  if (i > -1) array[i] = item;
-  else array.push(item);
-}
-const curr_email_ = [];
-app.post("/api/google-login", async (req, res) => {
-  const { token } = req.body;
-  const users = {};
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: CLIENT_ID,
-  });
-  // get the name email and picture of user, email should be unique key
-  const { name, email, picture, sub } = ticket.getPayload();
-  uid = sub;
-  users.name = name;
-  users.email = email;
-  users.picture = picture;
-  users.id = sub;
-  console.log(users);
-
-  // find id in db and see if it matches
-  MongoClient.connect(url, async function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("neufood");
-    var user = await dbo.collection("user").find({ uid: sub }).toArray();
-    if (user.length > 0) {
-      // user is matched
-      console.log("Welcome " + user[0].name);
-      console.log("Your email is " + user[0].email);
-    } else {
-      // new user, add to database
-      dbo.collection("user").insertOne({ uid: sub, name: name, email: email });
-      console.log("Welcome " + name + " to NeufoodAI!");
-      console.log("Thank you for making an account with us!");
-    }
-  });
-  res.send(users);
-});
-
-// logout for google-api
-app.get("/logout", (req, res) => {
-  // remove session cookie
-});
-
-/*
- * Middleware below
- */
-// verify with session cookie, if no session cookie, redirect to signin
-app.post("/auth", async function checkAuthenticated(req, res, next) {
-  let token = req.body.cookies;
-  let id = req.body.id;
-  /*async function getToken() {
-    // let token = req.cookies['session-token']; <- for some reason this code doesn't work, this would be better to use if can implement
-    try {
-      token = req.body.cookies.substring(30); // this is the offset from the direct cookie String
-    } catch {
-      token = "failure";
-    }
-  } 
-  await getToken(); */
-  let user = {};
-  async function verify() {
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID,
-    });
-    const payload = ticket.getPayload();
-    user.id = payload.sub;
-  }
-
-  verify()
-    .then(() => {
-      console.log("authenticated, let user through");
-      res.send({ data: true });
-    })
-    .catch((err) => {
-      onGoogleVerifyFailure();
-    });
-
-  const onGoogleVerifyFailure = () => {
-    try {
-      // Remove remnants of JSON.stringify
-      token = token.substr(1, token.length - 2);
-      id = id.substr(1, id.length - 2);
-
-      const payload = jwt.verify(token, process.env.JWTSECRET);
-
-      if (payload.id === id) {
-        console.log("authenticated, let user through");
-        res.send({ data: true });
-      } else {
-        console.log("not authenticated");
-        res.send({ data: false });
-      }
-    } catch (err) {
-      console.log("not authenticated");
-      res.send({ data: false });
-    }
-  };
-});
+// Mount the routes
+app.use("/allergies", allergiesRoutes);
+app.use("/badges", badgesRoutes);
 
 module.exports = app;
+
